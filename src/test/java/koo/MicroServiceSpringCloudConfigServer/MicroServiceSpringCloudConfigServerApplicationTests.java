0fccb53ce7e85f6177af75d963a7b986a5fcb841
rngustj9139 <mnb9139@khu.ac.kr>
2022-11-23 13:00:30 +0900

    First setting

---
+++ src/test/java/koo/MicroServiceSpringCloudConfigServer/MicroServiceSpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package koo.MicroServiceSpringCloudConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServiceSpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
